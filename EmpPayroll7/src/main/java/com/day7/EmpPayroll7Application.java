ea8e9b67b0fff965f27aa99d092f4fccce854739
BL-Abhisheak <abhisheak.bhaskaran@bridgelabz.com>
2026-02-03 11:20:21 +0530

    Initial Commit

---
+++ EmpPayroll7/src/main/java/com/day7/EmpPayroll7Application.java
@@ -0,0 +1,14 @@
+package com.day7;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmpPayroll7Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmpPayroll7Application.class, args);
+		
+	}
+
+}
---
+++ EmpPayroll7/src/test/java/com/day7/EmpPayroll7ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.day7;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmpPayroll7ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
